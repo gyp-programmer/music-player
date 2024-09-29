f420af6b5f9d79999f24864d0585947ba25a1c9e
grayson <grayson.gao@bvox.com>
2024-09-29 12:06:05 +0800

    feat: :art: 接入qiankun微应用

---
+++ music/micro.js
@@ -0,0 +1,18 @@
+((global) => {
+    global['music'] = {
+      bootstrap: () => {
+        return Promise.resolve();
+      },
+      mount: () => {
+        // console.log('music mount');
+        // render不需要写，页面所有的渲染都已用原声写完了，这里只是用来qiankun框架在运行loadApp 的时候，读取微应用的这个方法
+      },
+      unmount: () => {
+        console.log('music unmount');
+        return Promise.resolve();
+      },
+      update: () => {
+        return Promise.resolve();
+      }
+    };
+  })(window);
